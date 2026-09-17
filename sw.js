621b416a9ee0577e1059c9680eb8a5e60ceecf6a
rejaulalomkhan <armanazij@gmail.com>
2026-09-17 12:58:11 +0600

    feat: enforce HTTPS and enhance registration UI with new actions

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'arians-study-v3';
+const CACHE_NAME = 'arians-study-v4';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.svg', './icon-512.svg'];
 
 self.addEventListener('install', event => {
