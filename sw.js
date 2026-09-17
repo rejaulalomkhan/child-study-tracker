54fdf1eeed3527a9c365161decc86b384ef7b740
rejaulalomkhan <armanazij@gmail.com>
2026-09-17 11:08:46 +0600

    feat: update app cache version and include additional assets in service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'arians-study-v2';
-const APP_SHELL = ['./', './index.html', './manifest.webmanifest'];
+const CACHE_NAME = 'arians-study-v3';
+const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.svg', './icon-512.svg'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
